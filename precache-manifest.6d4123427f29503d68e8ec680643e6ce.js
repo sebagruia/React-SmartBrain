17bb6e4b9d36cbefa2fe04b54506b5979089e749
sebagruia <sebastiangruia@gmail.com>
2019-10-17 22:11:02 +0200

    Updates

---
+++ precache-manifest.6d4123427f29503d68e8ec680643e6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc2f7f146cfd27e150845961a6e10b7",
+    "url": "/React-SmartBrain/index.html"
+  },
+  {
+    "revision": "a3dc1e1a49d093b9db7d",
+    "url": "/React-SmartBrain/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f13df0001a4d7f0f90f7",
+    "url": "/React-SmartBrain/static/css/main.5732108f.chunk.css"
+  },
+  {
+    "revision": "a3dc1e1a49d093b9db7d",
+    "url": "/React-SmartBrain/static/js/2.96ad2b9f.chunk.js"
+  },
+  {
+    "revision": "f13df0001a4d7f0f90f7",
+    "url": "/React-SmartBrain/static/js/main.dd2412f7.chunk.js"
+  },
+  {
+    "revision": "cf26902c7a1fb0e329dc",
+    "url": "/React-SmartBrain/static/js/runtime-main.e44fb6b7.js"
+  },
+  {
+    "revision": "fca1f4f42893409bf4731c52c94020c4",
+    "url": "/React-SmartBrain/static/media/SmartBrain.fca1f4f4.png"
+  }
+]);
