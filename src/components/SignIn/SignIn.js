9b12bc613c306a8fc91f9d9a78c27c75785fd388
sebagruia <blackthorus@gmail.com>
2021-09-03 09:32:26 +0300

    Updated outdated npm packages, add .prettierrc file to source directory

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,81 +1,98 @@
-import React from 'react';
-import './SignIn.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import "./SignIn.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 class SignIn extends React.Component {
-
-  constructor({props}){
+  constructor({ props }) {
     super(props);
     this.state = {
-      signInEmail:'',
-      signInPassword:''
-    }
+      signInEmail: "",
+      signInPassword: "",
+    };
   }
 
-  onEmailChange = (event)=>{
-    this.setState({signInEmail:event.target.value});
-  }
-  onPasswordChange = (event)=>{
-    this.setState({signInPassword:event.target.value});
-  }
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
 
-  
-  onSignInButton = (event)=>{
-    event.preventDefault()
-      fetch('https://powerful-scrubland-05532.herokuapp.com/signIn', {
-        method:'post',
-        headers:{'Content-Type': 'application/json'},
-        body:JSON.stringify({
-          email:this.state.signInEmail,
-          password:this.state.signInPassword
-        })
-      })
-      .then(response=>response.json())
-      .then(user=>{
-        if(user.id){
+  onSignInButton = (event) => {
+    event.preventDefault();
+    fetch("https://powerful-scrubland-05532.herokuapp.com/signIn", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
           this.props.loadUser(user);
-          this.props.onRouteChange('home');
-        }else{
+          this.props.onRouteChange("home");
+        } else {
           window.alert("No such User!!!");
         }
       });
-  }
+  };
 
-  render(){
-    const {onRouteChange} = this.props;
+  render() {
+    const { onRouteChange } = this.props;
     return (
       <Card className="card-container m-auto" text="light">
-      <Card.Body border="light" >
-        <Card.Title>Sign In</Card.Title>
+        <Card.Body border="light">
+          <Card.Title>Sign In</Card.Title>
           <Form>
-              <Form.Group controlId="formBasicEmail">
+            <Form.Group controlId="formBasicEmail">
               <Form.Label className="label">Email address</Form.Label>
-              <Form.Control onChange = {this.onEmailChange} type="email" placeholder="Enter email" required />
-              <Form.Text className="light" >
-                  Don't worry I won't share your email, I'm not that guy.
+              <Form.Control
+                onChange={this.onEmailChange}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
+              <Form.Text className="text-light">
+                Don't worry I won't share your email, I'm not that guy.
               </Form.Text>
-              </Form.Group>
-          
-              <Form.Group controlId="formBasicPassword">
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
               <Form.Label className="label">Password</Form.Label>
-              <Form.Control onChange = {this.onPasswordChange} type="password" placeholder="Password" required/>
-              </Form.Group>
-              <div className="button-wrapper">
-                <Button onClick={this.onSignInButton} className="form-buttons" variant="primary" type="submit">Sign In</Button>
-                <Button  onClick={()=>onRouteChange('Register')} variant="link"  className="form-buttons register-button" type="submit">Register</Button>
-              </div>
-           
+              <Form.Control
+                onChange={this.onPasswordChange}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </Form.Group>
+            <div className="button-wrapper">
+              <Button
+                onClick={this.onSignInButton}
+                className="form-buttons"
+                variant="primary"
+                type="submit"
+              >
+                Sign In
+              </Button>
+              <Button
+                onClick={() => onRouteChange("Register")}
+                variant="link"
+                className="form-buttons register-button"
+                type="submit"
+              >
+                Register
+              </Button>
+            </div>
           </Form>
-      </Card.Body>
-    </Card>
-      
-   );
-
+        </Card.Body>
+      </Card>
+    );
   }
-
-
 }
 
 export default SignIn;
